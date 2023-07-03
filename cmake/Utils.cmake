5e5cac63ca326f202dd570c084172918fc6fa105
Adam Parker <adam.parker@ukaea.uk>
2023-07-03 10:31:09 +0100

    Fix cmake-format target

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -19,20 +19,9 @@ function(add_clang_format_target)
         if(${PROJECT_NAME}_BUILD_EXECUTABLE)
             add_custom_target(clang-format
                 COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-                -i ${exe_sources} ${headers}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
-        elseif(${PROJECT_NAME}_BUILD_HEADERS_ONLY)
-            add_custom_target(clang-format
-                COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-                -i ${headers}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
-        else()
-            add_custom_target(clang-format
-                COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-                -i ${sources} ${headers}
+                -i --verbose ${SOURCES} ${HEADERS} ${TEST_SOURCES}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
         endif()
-
         message(STATUS "Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).\n")
     endif()
 endfunction()
