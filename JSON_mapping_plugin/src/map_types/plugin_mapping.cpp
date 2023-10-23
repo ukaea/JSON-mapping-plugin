c9fad2fdd1334bbd81d4b5c2a489eccaa9ce7fb2
sdixon <stephen.dixon@ukaea.uk>
2023-10-23 23:31:10 +0100

    fixing unwanted dim scaling issues from initial testing

--- JSON_mapping_plugin/src/map_types/plugin_mapping.cpp
+++ JSON_mapping_plugin/src/map_types/plugin_mapping.cpp
@@ -8,6 +8,7 @@
 #include "utils/scale_offset.hpp"
 #include "utils/uda_plugin_helpers.hpp"
 #include <clientserver/stringUtils.h>
+#include <fmt/core.h>
 #include <inja/inja.hpp>
 #include <plugins/udaPlugin.h>
 #include <utils/ram_cache.hpp>
@@ -191,13 +192,14 @@ int PluginMapping::call_plugins(const MapArguments& arguments) const {
     
     // set serverside subsetting as default unless new method is specifically requested. 
     bool use_plugin_subset = (subset_method != nullptr) and (StringIEquals(subset_method, "PLUGIN_SUBSET")); 
+    // TODO: handle dim data scaling (hardcoded to disable scaling here)
+    bool dim_data = arguments.m_sig_type == SignalType::DIM or arguments.m_sig_type == SignalType::TIME;
 
     if (datasubset.nbound > 0 and use_plugin_subset)
     {
-        // TODO: m_scale not populated when expected here...
-        auto scale_value = m_scale.has_value() ? m_scale.value() : 1.0;
+        auto scale_value = (!dim_data and m_scale.has_value()) ? m_scale.value() : 1.0;
         subset::log(subset::LogLevel::INFO, "scale factor is: " + std::to_string(scale_value));
-        auto offset_value = m_offset.has_value() ? m_offset.value() : 0.0;
+        auto offset_value = (!dim_data and  m_offset.has_value()) ? m_offset.value() : 0.0;
         subset::log(subset::LogLevel::INFO, "offset factor is: " + std::to_string(offset_value));
         subset::apply_subsetting(arguments.m_interface, scale_value, offset_value);
         
@@ -212,6 +214,16 @@ int PluginMapping::call_plugins(const MapArguments& arguments) const {
         // if using serverside subsetting then this is the option to reduce 
         // rank when a dim length is only 1. i.e. vector -> scaler for 1d slice.
         arguments.m_interface->request_data->datasubset.reform = 1;
+
+        if (m_plugin == "UDA" and arguments.m_sig_type == SignalType::TIME) {
+            // Opportunity to handle time differently
+            // Return time SignalType early, no need to scale/offset
+            if (!cache_hit)
+            {
+                err = imas_json_plugin::uda_helpers::setReturnTimeArray(arguments.m_interface->data_block);
+            }
+            return err;
+        }
 
         /*
          * scale and subset added to subset routine above 
@@ -228,12 +240,6 @@ int PluginMapping::call_plugins(const MapArguments& arguments) const {
         if (m_offset.has_value())
         {
             err = JMP::map_transform::transform_offset(arguments.m_interface->data_block, m_offset.value());
-        }
-        if (m_plugin == "UDA" && arguments.m_sig_type == SignalType::TIME) {
-            // Opportunity to handle time differently
-            // Return time SignalType early, no need to scale/offset
-            err = imas_json_plugin::uda_helpers::setReturnTimeArray(arguments.m_interface->data_block);
-            return err;
         }
     }
 
